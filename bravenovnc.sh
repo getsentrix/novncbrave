fda832cde7f15e4538ab9ad0a3fcc615b8b98418
getsentrix <215050518+getsentrix@users.noreply.github.com>
2025-11-01 17:33:14 -0500

    Update bravenovnc.sh

--- bravenovnc.sh
+++ bravenovnc.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+export DISPLAY=:1
+
+Xvfb :1 -screen 0 1280x720x24 &
+
+x11vnc -display :1 -nopw -forever -shared &
+
+openbox &
+
+dbus-launch brave-browser --no-sandbox --disable-gpu &
+  
+wait
